c3da69f9e08bd93de68e99c21f36fc9704c0e9ad
Ordnas1 <sandromarcelo.peirano@gmail.com>
2020-04-11 21:40:31 -0500

    Add reset button and refactor grid code

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,58 @@
 /* Main JS script
 *  fill the grid and handles grid effects */
 
-let gridTotal = 16 * 16;
-
 const gridContainer = document.querySelector('#grid-container');
 
-for (let i = 0; i < gridTotal ; i++) {
-    let gridElem = document.createElement('div');
-    gridElem.classList.add('grid-elem');
-    gridContainer.appendChild(gridElem);
+function loadGrid(gridSideLength) {
+    let gridTotal = gridSideLength ** 2;
+    
+
+    for (let i = 0; i < gridTotal ; i++) {
+        let gridElem = document.createElement('div');
+        gridElem.classList.add('grid-elem');
+        gridContainer.appendChild(gridElem);
+    }
 }
 
-
+const ORIGINALGRIDSIDE = 16;
+loadGrid(ORIGINALGRIDSIDE)
 
 function changeGridColor(e) {
     e.target.style.cssText = "background-color: black;";
 }
 
 
-const gridElements = document.querySelectorAll('.grid-elem');
-
-gridElements.forEach((elem) => {
-    elem.addEventListener('mouseover', (e) => {
-        changeGridColor(e)
+let gridElements; 
+function createGridElemEvent() {
+    gridElements = document.querySelectorAll('.grid-elem');
+    gridElements.forEach((elem) => {
+        elem.addEventListener('mouseover', (e) => {
+            changeGridColor(e)
+        })
     })
+}
+
+createGridElemEvent()
+
+function deleteChild() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild)
+    }
+}
+
+function recalcGrid(gridLength) {
+    gridContainer.setAttribute('style',`grid-template-columns: repeat(${gridLength}, 1fr);grid-template-rows: repeat(${gridLength}, 1fr);`)
+}
+
+function resetGrid(gridLength) {
+    deleteChild();
+    recalcGrid(gridLength);
+    loadGrid(gridLength);
+}
+
+const resetBtn = document.querySelector("#reset-btn");
+resetBtn.addEventListener('click', () => {
+    let newGridLength = window.prompt("Enter your new grid side length");
+    resetGrid(newGridLength)
+    createGridElemEvent()
 })
